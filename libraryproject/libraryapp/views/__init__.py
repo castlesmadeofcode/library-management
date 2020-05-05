62b056659ed10f7d438efb8175332823706b9abd
castle <castlesmadeofcode@gmail.com>
2020-05-05 13:15:45 -0500

    created detail view for books

--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -1,5 +1,6 @@
 from .books.list import book_list
 from .books.form import book_form
+from .books.details import book_details
 from .librarians.list import list_librarians
 from .libraries.form import library_form
 from .libraries.list import library_list
