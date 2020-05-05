e2e2a55c9a71ebbe46c80bc65a1755e250788459
castle <castlesmadeofcode@gmail.com>
2020-05-05 15:32:41 -0500

    Book 3 Chater 6 - Practice Library Details, Librarian Details complete

--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -2,7 +2,9 @@ from .books.list import book_list
 from .books.form import book_form
 from .books.details import book_details
 from .librarians.list import list_librarians
+from .librarians.details import librarian_details
 from .libraries.form import library_form
 from .libraries.list import library_list
+from .libraries.details import library_details
 from .auth.logout import logout_user
 from .home import home
--- libraryproject/libraryapp/views/libraries/details.py
+++ libraryproject/libraryapp/views/libraries/details.py
@@ -2,40 +2,35 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from libraryapp.models import Book, Library
+from libraryapp.models import Library
 from libraryapp.models import model_factory
 from ..connection import Connection
 
 
-def get_book(book_id):
+def get_library(library_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Book)
+        conn.row_factory = model_factory(Library)
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
         SELECT
             b.id,
             b.title,
-            b.isbn,
-            b.author,
-            b.publisher,
-            b.year_published,
-            b.librarian_id,
-            b.location_id
-        FROM libraryapp_book b
+            b.address
+        FROM libraryapp_library b
         WHERE b.id = ?
-        """, (book_id,))
+        """, (library_id,))
 
         return db_cursor.fetchone()
 
 @login_required
-def book_details(request, book_id):
+def library_details(request, library_id):
     if request.method == 'GET':
-        book = get_book(book_id)
+        library = get_library(library_id)
 
-        template = 'books/detail.html'
+        template = 'libraries/detail.html'
         context = {
-            'book': book
+            'library': library
         }
 
         return render(request, template, context)
